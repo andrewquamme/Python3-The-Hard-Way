e5a2f6e2d641d75fe0767c1c04c4e2a8142b2ca4
andrewquamme <andrewquamme@gmail.com>
2019-06-06 20:54:21 -0700

    exercise 35 fix typos

--- ex35.py
+++ ex35.py
@@ -28,7 +28,7 @@ def bear_room():
         choice = input("> ")
 
         if choice == "take honey":
-            dead("The bear looks at ou then slaps your face off.")
+            dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
             print("The bear has moved from the door.")
             print("You can go through it now.")
